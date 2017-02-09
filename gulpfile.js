0491e04a5cb5f95d061e66e853328b9e79c6713f
Gabriel M <gabrieletudes@gmail.com>
2017-02-09 15:42:25 +0100

    Setup gulp env

---
+++ gulpfile.js
@@ -0,0 +1,23 @@
+/* gabriel/katbiznis
+ *
+ * /gulpfile.js - Gulp tasks
+ *
+ *
+ * coded by gabriel!
+ *
+ * started at 09/02/2017
+ */
+
+ var gulp = require( "gulp" );
+
+// --- Task for images
+
+// --- Task for styles
+
+// --- Task for pug
+
+// --- Task for js
+
+// --- Watch tasks
+
+// --- Aliases
